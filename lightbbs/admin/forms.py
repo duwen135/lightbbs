9b25f759df01ff1b366e692e51af8bfaee3dd622
duwen135 <duwen135@163.com>
2018-03-19 18:04:59 +0800

    新增一堆东西

--- lightbbs/admin/forms.py
+++ lightbbs/admin/forms.py
@@ -8,6 +8,7 @@ from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from ..models.role import Role
 from ..models.user import User
+from ..models.node import Node
 
 
 class UserEdit(FlaskForm):
@@ -42,3 +43,37 @@ class UserEdit(FlaskForm):
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('该用户名已被注册')
+
+
+class NodeForm(FlaskForm):
+    name = StringField('节点名称', validators=[
+        DataRequired, Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                            'Usernames must have only letters, '
+                                            'numbers, dots or underscores')])
+    parent_id = SelectField('父目录', coerce=int)
+    keywords = StringField('节点关键词')
+    content = TextAreaField('分类简介')
+    master_id = StringField('节点版主')
+
+    def __init__(self, *args, **kwargs):
+        super(NodeForm, self).__init__(*args, **kwargs)
+        self.parent_id.choices = [(node.id, node.name)
+                             for node in Node.query.order_by(Node.name).filter_by(parent_id=0).all()]
+        #self.node = node
+
+    def validate_name(self, field):
+        if Node.query.filter_by(name=field.data).first():
+            raise ValidationError('该节点已存在!')
+
+
+class TopicForm(FlaskForm):
+    node = SelectField('请选择节点', choices=[], coerce=int)
+    title = StringField('请输入话题标题', validators=[DataRequired()])
+    content = TextAreaField('描述您的内容', validators=[DataRequired()])
+    keywords = StringField('话题关键词，多个请用逗号隔开')
+    submit = SubmitField('提交')
+
+class PageForm(FlaskForm):
+    title = StringField('网站名称', validators=[DataRequired])
+    go_url = StringField('网址', validators=[DataRequired])
+    is_hidden = BooleanField('是否隐藏', validators=[DataRequired])
--- lightbbs/admin/views.py
+++ lightbbs/admin/views.py
@@ -5,10 +5,15 @@ from flask import render_template, request, redirect, url_for, flash, current_ap
 from . import admin
 from flask_login import current_user, login_required
 from ..decorators import admin_required, permission_required
+from datetime import datetime
 from ..import db
-from .forms import UserEdit
+from .forms import UserEdit, NodeForm, TopicForm
 from ..models.user import User
 from ..models.role import Role
+from ..models.node import Node
+from ..models.topic import Topic
+from ..models.link import Link
+from ..models.page import Page
 
 
 #用户管理
@@ -23,7 +28,7 @@ def user_list():
     users = pagination.items
     return render_template('/admin/users.html', users=users)
 
-@admin.route('/edit/<id>', methods=['GET', 'POST'])
+@admin.route('/edit/<int:id>', methods=['GET', 'POST'])
 @admin_required
 @login_required
 def user_edit(id):
@@ -54,7 +59,7 @@ def user_edit(id):
     form.role.data = user.role_id
     return render_template('/admin/user_edit.html', form=form, user=user)
 
-@admin.route('/delete/<id>', methods=['POST'])
+@admin.route('/user_delete/<int:id>', methods=['POST'])
 @admin_required
 @login_required
 def user_delete(id):
@@ -62,3 +67,178 @@ def user_delete(id):
     db.session.delete(user)
     flash('删除成功！')
     render_template('/admin/users.html', user=user)
+
+#节点管理
+@admin.route('/nodes')
+@admin_required
+@login_required
+def node_list():
+    parent_nodes = Node.query.filter_by(parent_id=0).all()
+
+    def get_node(parent_node):
+        nodes = Node.query.filter_by(parent_id=parent_node.id).all()
+        return nodes
+
+    return render_template('admin/nodes.html', parent_nodes=parent_nodes, get_node=get_node)
+
+@admin.route('/node_add', methods=['GET', 'POST'])
+@admin_required
+@login_required
+def node_add():
+    form = NodeForm()
+    if form.validate_on_submit():
+        node = Node(
+            name=form.name.data,
+            parent_id=form.parent_id.data,
+            keywords=form.keywords.data,
+            content=form.content.data,
+            master_id=form.master_id.data
+        )
+        db.session.add(node)
+        flash('节点创建成功！')
+        return redirect(url_for('admin.node_list'))
+    return render_template('admin/node_add.html', form=form)
+
+@admin.route('/node_edit/<int:id>', methods=['GET', 'POST'])
+@admin_required
+@login_required
+def node_edit(id):
+    node = Node.query.get_or_404(id)
+    form = NodeForm(node=node)
+    if form.validate_on_submit():
+        node.name = form.name.data,
+        node.parent_id = form.parent_id.data,
+        node.keywords = form.keywords.data,
+        node.content = form.content.data,
+        node.master_id = form.master_id.data
+
+        db.session.add(node)
+        flash('节点修改成功！')
+        return redirect(url_for('admin.node_list'))
+    return render_template('admin/node_edit.html', node=node, form=form)
+
+@admin.route('/node_delete/<int:id>', methods=['POST'])
+@admin_required
+@login_required
+def node_delete(id):
+    node = Node.query.get_or_404(id)
+    db.session.delete(node)
+    flash('节点删除成功!')
+    return render_template('admin/node_list.html', node=node)
+
+
+#话题管理
+@admin.route('/topic_list')
+@admin_required
+@login_required
+def topic_list():
+    page = request.args.get('page', 1, type=int)
+    pagination = Topic.query.order_by(Topic.addtime.desc()).paginate(
+        page, per_page=current_app.config['LIGHTBBS_POSTS_PER_PAGE'],
+        error_out=False)
+    topics = pagination.items
+    return render_template('/admin/topics.html', topics=topics)
+
+@admin.route('/topic_add', methods=['GET', 'POST'])
+@admin_required
+@login_required
+def topic_add():
+    form = TopicForm()
+    form.node.choices = [(n.id, n.name) for n in Node.query.order_by(Node.id).all()]
+    if form.validate_on_submit():
+        topic = Topic(node_id=form.node.data,
+                      sender_id=current_user._get_current_object().id,
+                      title=form.title.data,
+                      content=form.content.data,
+                      keywords=form.keywords.data,
+                      addtime=datetime.utcnow())
+        db.session.add(topic)
+        flash('话题发布成功！')
+        return redirect(url_for('admin.topics'))
+    return render_template('/admin/topic_add.html', form=form)
+
+@admin.route('/topic_edit/<int:id>', methods=['GET', 'POST'])
+@admin_required
+@login_required
+def topic_edit(id):
+    topic = Topic.query.get_or_404(id)
+    form = TopicForm()
+    form.node.choices = [(n.id, n.name) for n in Node.query.order_by(Node.id).all()]
+    if form.validate_on_submit():
+        topic.node_id = form.node.data
+        topic.title = form.title.data
+        topic.content = form.content.data
+        topic.keywords = form.keywords.data
+        topic.update_time = datetime.utcnow()
+        db.session.add(topic)
+        flash('这篇文章已经更新了。')
+        return redirect(url_for('admin.topics', id=topic.id))
+    form.node.data = topic.node_id
+    form.title.data = topic.title
+    form.content.data = topic.content
+    form.keywords.data = topic.keywords
+    return render_template('/admin/topic_edit.html', form=form)
+
+@admin.route('/topic_delete/<int:id>', methods=['POST'])
+@admin_required
+@login_required
+def topic_delete(id):
+    topic = Topic.query.get_or_404(id)
+    db.session.delete(topic)
+    flash('删除成功!')
+    return render_template('/admin/topics.html', topic=topic)
+
+@admin.route('/topic_top/<int:id>', methods=['POST'])
+@admin_required
+@login_required
+def top(id):
+    topic = Topic.query.get_or_404(id)
+    topic.is_top = True
+    db.session.add(topic)
+    flash('置顶成功！')
+    return render_template('/admin/topics.html', topic=topic)
+
+@admin.route('/topic_untop/<int:id>', methods=['POST'])
+@admin_required
+@login_required
+def untop(id):
+    topic = Topic.query.get_or_404(id)
+    topic.is_top = False
+    db.session.add(topic)
+    flash('取消置顶！')
+    return render_template('/admin/topics.html', topic=topic)
+
+@admin.route('/topic_hidden/<int:id>', methods=['POST'])
+@admin_required
+@login_required
+def hidden(id):
+    topic = Topic.query.get_or_404(id)
+    topic.is_hidden = True
+    db.session.add(topic)
+    flash('已设置隐藏！')
+    return render_template('/admin/topics.html', topic=topic)
+
+@admin.route('/topic_unhidden/<int:id>', methods=['POST'])
+@admin_required
+@login_required
+def unhidden(id):
+    topic = Topic.query.get_or_404(id)
+    topic.is_hidden = False
+    db.session.add(topic)
+    flash('已设置显示！')
+    return render_template('/admin/topics.html', topic=topic)
+
+
+#友情链接
+
+#单页
+@admin.route('/page_list')
+@admin_required
+@login_required
+def page_list():
+    page = request.args.get('page', 1, type=int)
+    pagination = Page.query.order_by(Page.add_time.desc()).paginate(
+        page, per_page=current_app.config['LIGHTBBS_POSTS_PER_PAGE'],
+        error_out=False)
+    pages = pagination.items
+    return render_template('/admin/topics.html', pages=pages)
